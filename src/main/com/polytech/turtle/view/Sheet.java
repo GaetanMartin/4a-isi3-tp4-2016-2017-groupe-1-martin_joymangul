d07f30c6dfdbe31e9732cbb07b4866de6ab2602a
Gaetan Martin <GaetanMartin>
2017-05-17 08:29:31 +0200

    Optimized turtles drawing

--- src/main/com/polytech/turtle/view/Sheet.java
+++ src/main/com/polytech/turtle/view/Sheet.java
@@ -6,10 +6,8 @@ import com.polytech.turtle.view.events.MouseAdapterCustom;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Titre :        Logo
@@ -25,18 +23,22 @@ public class Sheet extends JPanel implements Observer {
 
 	public static final int DEFAULT_HEIGHT = 400;
 
-	private List<Turtle> turtles; // la liste des turtles enregistrees
-	
+	private List<Turtle> turtles = new ArrayList<>(); // la liste des turtles enregistrees
+	private Map<Turtle, TurtleView> turtleViewMap = new HashMap<>();
+
+
 	public Sheet() {
-		turtles = new ArrayList<>();
 		this.setBackground(Color.white);
 		this.setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 		this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 	}
 
-	public void addTortue(Turtle o) {
-		o.addObserver(this);
-		turtles.add(o);
+	public void addTortue(Turtle turtle) {
+		turtle.addObserver(this);
+		turtles.add(turtle);
+		TurtleView turtleView = new TurtleView(turtle);
+		turtleViewMap.put(turtle, turtleView);
+		this.addMouseListener(new MouseAdapterCustom(turtle, turtleView));
 		repaint();
 	}
 
@@ -58,33 +60,32 @@ public class Sheet extends JPanel implements Observer {
 		g.fillRect(0,0,dim.width, dim.height);
         g.setColor(c);
 
-		showTurtles(g);
+		drawTurtles(g);
 	}
 
-	public void showTurtles(Graphics g) {
-		for(Turtle t : turtles) {
-            g.setColor(t.getColor());
-			TurtleView turtleView = new TurtleView(t);
-			Polygon arrow = turtleView.getShape();
-			this.addMouseListener(new MouseAdapterCustom(t, turtleView));
-			g.fillPolygon(arrow);
-			drawSegments(g, t.getListSegments());
-		}
+	private void drawTurtles(Graphics g) {
+		turtles.forEach((Turtle turtle) -> drawTurtle(turtle, g));
 	}
 
-	public void drawSegments(Graphics graph, ArrayList<Segment> listSegment)
+	private void drawTurtle(Turtle t, Graphics g) {
+		Polygon arrow = turtleViewMap.get(t).getShape();
+		g.setColor(t.getColor());
+		g.fillPolygon(arrow);
+		drawSegments(g, t.getListSegments());
+	}
+
+	private void drawSegments(Graphics graph, ArrayList<Segment> listSegment)
 	{
-		listSegment.stream().forEach((Segment segment) -> drawSegment(graph, segment));
+		listSegment.forEach((Segment segment) -> drawSegment(graph, segment));
 	}
 
-	public void drawSegment(Graphics graph, Segment segment) {
+	private void drawSegment(Graphics graph, Segment segment) {
         graph.setColor(segment.getColor());
 		graph.drawLine(segment.getStart().getX(), segment.getStart().getY(), segment.getEnd().getX(), segment.getEnd().getY());
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		System.out.println("Repaint");
         revalidate();
 		repaint();
 	}
